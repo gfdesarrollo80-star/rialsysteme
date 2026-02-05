cc63b9a5a9127c7dedce103659725251f6f34062
gfdesarrollo80-star <gfdesarrollo80@gmail.com>
2026-02-05 01:14:59 -0300

    Update users.routes.js

--- backend/src/routes/users.routes.js
+++ backend/src/routes/users.routes.js
@@ -3,12 +3,18 @@ import { verifyToken } from "../middlewares/auth.js";
 import {
   getUsers,
   getUserById,
+  createUser,
+  updateUser,
+  deleteUser
 } from "../controllers/users.controller.js";
 
 const router = Router();
 
-// rutas protegidas
+// 🔐 RUTAS PROTEGIDAS
 router.get("/", verifyToken, getUsers);
 router.get("/:id", verifyToken, getUserById);
+router.post("/", verifyToken, createUser);
+router.put("/:id", verifyToken, updateUser);
+router.delete("/:id", verifyToken, deleteUser);
 
 export default router;
