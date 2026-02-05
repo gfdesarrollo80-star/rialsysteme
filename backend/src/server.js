2fd70d1264c3afd04a16c9e8f872363dfc26adf1
gfdesarrollo80-star <gfdesarrollo80@gmail.com>
2026-02-05 00:59:26 -0300

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,3 @@
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-import router from "./routes/index.js";
+import usersRoutes from "./routes/users.routes.js";
 
-dotenv.config();
-
-const app = express();
-app.use(cors());
-app.use(express.json());
-
-app.use("/api", router);
-
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
-  console.log(`🔥 Servidor Tesorería ejecutándose en puerto ${PORT}`);
-});
+app.use("/api/usuarios", usersRoutes);
