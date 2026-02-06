9091831d083bd7526d4aba8ece58eb06c4fc1aa9
gfdesarrollo80-star <gfdesarrollo80@gmail.com>
2026-02-06 11:07:30 -0300

    Update admin.routes.js

--- backend/src/routes/admin.routes.js
+++ backend/src/routes/admin.routes.js
@@ -2,15 +2,29 @@ import { Router } from "express";
 import { verifyToken } from "../middlewares/auth.js";
 import { requireRole } from "../middlewares/role.middleware.js";
 
+import {
+  getUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+} from "../controllers/users.controller.js";
+
 const router = Router();
 
-router.get(
-  "/admin/data",
-  verifyToken,
-  requireRole([1]), // solo admin
-  (req, res) => {
-    res.json({ mensaje: "Datos solo para admin" });
-  }
-);
+// Middleware admin global
+router.use(verifyToken, requireRole([1]));
+
+// Test admin
+router.get("/admin/data", (req, res) => {
+  res.json({ mensaje: "Datos solo para admin" });
+});
+
+// CRUD USUARIOS (ADMIN)
+router.get("/admin/users", getUsers);
+router.get("/admin/users/:id", getUserById);
+router.post("/admin/users", createUser);
+router.put("/admin/users/:id", updateUser);
+router.delete("/admin/users/:id", deleteUser);
 
 export default router;
