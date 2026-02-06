4416852fc69cdfd566c26811bcc59c18edebf598
gfdesarrollo80-star <gfdesarrollo80@gmail.com>
2026-02-05 23:42:47 -0300

    Update Dashboard.jsx

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,6 +1,19 @@
+import { useEffect } from "react";
+import api from "../api/axios";
+
 export default function Dashboard() {
   const nombre = localStorage.getItem("nombre");
   const rol = localStorage.getItem("rol");
+
+  useEffect(() => {
+    api.get("/users")
+      .then((res) => {
+        console.log("USERS:", res.data);
+      })
+      .catch((err) => {
+        console.error("ERROR API:", err);
+      });
+  }, []);
 
   return (
     <div style={{ padding: 40 }}>
