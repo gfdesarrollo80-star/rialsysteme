e26a302c5e1871004f85eaf0f8bfe1c65ba3e4b3
gfdesarrollo80-star <gfdesarrollo80@gmail.com>
2026-02-06 10:08:51 -0300

    Update ProtectedRoute.jsx

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,11 +1,12 @@
 import { Navigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
 
 export default function ProtectedRoute({ children }) {
-  const token = localStorage.getItem("token");
+  const { user, loading } = useAuth();
 
-  if (!token) {
-    return <Navigate to="/" replace />;
-  }
+  if (loading) return <p>Cargando...</p>;
+
+  if (!user) return <Navigate to="/login" />;
 
   return children;
 }
