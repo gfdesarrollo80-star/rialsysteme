5355c730d81877b66d126dc4bdde5bfa20af17f7
gfdesarrollo80-star <gfdesarrollo80@gmail.com>
2026-02-05 23:55:30 -0300

    Create admin.routes.js

---
+++ backend/src/routes/admin.routes.js
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import { verifyToken } from "../middlewares/auth.middleware.js";
+import { requireRole } from "../middlewares/role.middleware.js";
+
+const router = Router();
+
+router.get(
+  "/admin/data",
+  verifyToken,
+  requireRole([1]), // solo admin
+  (req, res) => {
+    res.json({ mensaje: "Datos solo para admin" });
+  }
+);
+
+export default router;
