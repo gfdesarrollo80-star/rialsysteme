a5ec1d9a52ed89193529c06dd6545ef1819db525
gfdesarrollo80-star <gfdesarrollo80@gmail.com>
2026-02-06 11:19:00 -0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,10 @@
 import { Routes, Route } from "react-router-dom";
+
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import Admin from "./pages/Admin";
+import AdminUsers from "./pages/AdminUsers";
+
 import ProtectedRoute from "./components/ProtectedRoute";
 import ProtectedRouteByRole from "./components/ProtectedRouteByRole";
 import DashboardLayout from "./layout/DashboardLayout";
@@ -9,8 +12,10 @@ import DashboardLayout from "./layout/DashboardLayout";
 export default function App() {
   return (
     <Routes>
+      {/* LOGIN */}
       <Route path="/login" element={<Login />} />
 
+      {/* RUTAS PROTEGIDAS */}
       <Route
         element={
           <ProtectedRoute>
@@ -20,11 +25,22 @@ export default function App() {
       >
         <Route path="/dashboard" element={<Dashboard />} />
 
+        {/* ADMIN HOME */}
         <Route
           path="/admin"
           element={
-            <ProtectedRouteByRole role="admin">
+            <ProtectedRouteByRole role={1}>
               <Admin />
+            </ProtectedRouteByRole>
+          }
+        />
+
+        {/* ADMIN - USUARIOS */}
+        <Route
+          path="/admin/users"
+          element={
+            <ProtectedRouteByRole role={1}>
+              <AdminUsers />
             </ProtectedRouteByRole>
           }
         />
