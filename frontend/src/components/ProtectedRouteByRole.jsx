12ed53179f8189997725c9124bbc9ccc3a00eb9b
gfdesarrollo80-star <gfdesarrollo80@gmail.com>
2026-02-06 10:09:30 -0300

    Update ProtectedRouteByRole.jsx

--- frontend/src/components/ProtectedRouteByRole.jsx
+++ frontend/src/components/ProtectedRouteByRole.jsx
@@ -1,17 +1,13 @@
 import { Navigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
 
-export default function ProtectedRouteByRole({ children, allowedRoles }) {
-  const token = localStorage.getItem("token");
-  const rol = Number(localStorage.getItem("rol"));
+export default function ProtectedRouteByRole({ children, role }) {
+  const { user, loading } = useAuth();
 
-  // Si no hay token → login
-  if (!token) {
-    return <Navigate to="/" replace />;
-  }
+  if (loading) return <p>Cargando...</p>;
 
-  // Si el rol NO está permitido → dashboard
-  if (!allowedRoles.includes(rol)) {
-    return <Navigate to="/dashboard" replace />;
+  if (!user || user.role !== role) {
+    return <Navigate to="/dashboard" />;
   }
 
   return children;
