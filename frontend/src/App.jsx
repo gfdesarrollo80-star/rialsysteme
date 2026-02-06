ec62f20af9a89e46d33864068f7727b8bc7bc71a
gfdesarrollo80-star <gfdesarrollo80@gmail.com>
2026-02-06 10:29:44 -0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,37 +1,34 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import Admin from "./pages/Admin";
-
 import ProtectedRoute from "./components/ProtectedRoute";
 import ProtectedRouteByRole from "./components/ProtectedRouteByRole";
+import DashboardLayout from "./layout/DashboardLayout";
 
-function App() {
+export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
+    <Routes>
+      <Route path="/login" element={<Login />} />
 
-        <Route
-          path="/dashboard"
-          element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          }
-        />
+      <Route
+        element={
+          <ProtectedRoute>
+            <DashboardLayout />
+          </ProtectedRoute>
+        }
+      >
+        <Route path="/dashboard" element={<Dashboard />} />
 
         <Route
           path="/admin"
           element={
-            <ProtectedRouteByRole allowedRoles={[1]}>
+            <ProtectedRouteByRole role="admin">
               <Admin />
             </ProtectedRouteByRole>
           }
         />
-      </Routes>
-    </BrowserRouter>
+      </Route>
+    </Routes>
   );
 }
-
-export default App;
